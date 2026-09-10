18097eee7c528fa7ce0beeefc5f058b6df6d8fab
AoyonChowdhury <aoyonchowdhury01@gmail.com>
2026-09-11 04:56:37 +0600

    Technology section added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import Banner from "./components/Banner"
-import Navbar from "./components/Navbar"
-
+import { Suspense } from "react";
+import Banner from "./components/Banner";
+import Navbar from "./components/Navbar";
+import TechnologyCards from "./components/TechnologyCards";
+import type { ITechnologyType } from "./Types/ITechnologyType";
+const technologyFetch = async ():Promise<ITechnologyType> => {
+  const res = fetch("/data.json");
+  const data = (await res).json();
+  return data;
+};
 function App() {
-
+  const technologyPromise = technologyFetch();
   return (
     <>
-     <Navbar></Navbar>
-     <Banner></Banner>
+      <Navbar></Navbar>
+      <Banner></Banner>
+      <Suspense fallback = {<p>Loading......</p>}>
+        <TechnologyCards technologyPromise = {technologyPromise}></TechnologyCards>
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
